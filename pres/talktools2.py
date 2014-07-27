60e4ad2563553e0867f6d888caa3c04483a54012
etpinard <etienne.t.pinard@gmail.com>
2014-07-26 21:08:46 -0400

    second commit

---
+++ pres/talktools2.py
@@ -0,0 +1,36 @@
+"""Tools to style a talk."""
+
+from IPython.display import HTML, display, YouTubeVideo, Image
+
+
+def prefix(url):
+    prefix = '' if url.startswith('http') else 'http://'
+    return prefix + url
+
+
+def simple_link(url, name=None):
+    name = url if name is None else name
+    url = prefix(url)
+    return '<a href="%s" target="_blank">%s</a>' % (url, name)
+
+
+def html_link(url, name=None):
+    return HTML(simple_link(url, name))
+
+
+# Utility functions
+def website(url, name=None, width=700, height=500):
+    html = []
+    if name:
+        html.extend(['<div class="nb_link">',
+                     simple_link(url, name),
+                     '</div>'] )
+
+    html.append('<iframe src="%s"  width="%s" height="%s">' % 
+                (prefix(url), width, height))
+    return HTML('\n'.join(html))
+
+# Load and publish CSS
+style = HTML(open('style.css').read())
+
+display(style)
